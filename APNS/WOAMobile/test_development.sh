a8704319e1aaa9eec1767d1483b4311835389428
Steven Zhuang <lifejourney@126.com>
2016-09-15 10:54:10 +0800

    Update resources.

---
+++ APNS/WOAMobile/test_development.sh
@@ -0,0 +1,2 @@
+openssl s_client -connect gateway.sandbox.push.apple.com:2195 -cert aps_development_cer.pem -key aps_development_key.pem
+
---
+++ APNS/WOAMobile/test_production.sh
@@ -0,0 +1,1 @@
+openssl s_client -connect gateway.push.apple.com:2195 -cert aps_production_cer.pem -key aps_production_key.pem
